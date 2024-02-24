ec78a85d83cdd1d6f5a11847113c42793cd61e67
genchsusu <pctxiaozhu@gmail.com>
2024-02-24 16:12:18 +0800

    feat: fix plants issue

--- main/modules/farm_plant.lua
+++ main/modules/farm_plant.lua
@@ -7,39 +7,10 @@ local enable_all_seasons_growth = GetModConfigData('enable_all_seasons_growth')
 local PlantDefs = require("prefabs/farm_plant_defs").PLANT_DEFS
 
 local all_seasons = {autumn = true, winter = true, spring = true, summer = true}
-local function SetAllSeasonsGrowth(prefab)
-    prefab.good_seasons = all_seasons
-end
-
-local function DoModification(prefab)
-    AddPrefabPostInit(prefab, function(inst)
-        -- dig_up
-        if inst.components.workable then
-            local old_onfinish = inst.components.workable.onfinish
-            inst.components.workable.onfinish = function(...)
-                SpawnPrefab("farm_soil").Transform:SetPosition(inst.Transform:GetWorldPosition())
-                old_onfinish(...)
-            end
-        end
-
-        -- burnt
-        if inst.components.burnable then
-            local old_onburnt = inst.components.burnable.onburnt
-            inst.components.burnable:SetOnBurntFn(function(...)
-                SpawnPrefab("farm_soil").Transform:SetPosition(inst.Transform:GetWorldPosition())
-                old_onburnt(...)
-            end)
-        end
-    end)
-end
-
 for _, v in pairs(PlantDefs) do
     if v.prefab then
         if enable_all_seasons_growth then
-            SetAllSeasonsGrowth(v)
-        end
-        if not v.is_randomseed then
-            DoModification(v.prefab)
+            v.good_seasons = all_seasons
         end
     end
 end
--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ end
 
 name = e_or_z("Basements", "地下室")
 author = "OpenSource"
-version = "1.0.14"
+version = "1.0.15"
 
 description =e_or_z(
     [[
